efcdb8b0455ae4499dae5036852be1de08f4d347
mbenkhayal <mbenkhayal@users.noreply.github.com>
2015-11-12 14:47:14 +0000

    finished pull relations script

--- relationship-calculations/pull-relationships.py
+++ relationship-calculations/pull-relationships.py
@@ -9,29 +9,28 @@ relationships = client['ossfinder']['rel_pulls']
 
 relationships.delete_many({})
 
-names = pulls.distinct('full_name')
+users = pulls.distinct('user.login')
 
-pullInfo = {}
+for user in users:
+	userPulls = pulls.find({'user.login': user}).distinct("full_name")
+	if(len(userPulls) > 1):
+		for i in range(0, len(userPulls)):
+			for j in range(i+1, len(userPulls)):
+				rel = relationships.find({'repo_a': userPulls[i], 'repo_b': userPulls[j]})
+				rel = list(rel)
+				if(len(rel) > 0):
+					rel = rel[0]
+					rel['users'] = rel['users'] + 1
+					out = str(['U', userPulls[i], userPulls[j]])
+					print(out)
+					output.write(out)
+					relationships.update_one({'_id':rel['_id']}, {'$inc':{'pulls':1}})
+				else:
+					rel = {'repo_a':userPulls[i], 'repo_b': userPulls[j], 'pulls': 1}
+					out = str(['C', userPulls[i], userPulls[j]])
+					print(out)
+					output.write(out)
+					relationships.save(rel)
 
-for name in names:
-	userInfo = name.split("/")
-	if (pullInfo.has_key(userInfo[0]):
-		#person already has another pull, create relation
-	else:
-		#person doesn't have another pull, add to the dict
-		
-
-
-
-
-
-
-'''
-usernames = []
-
-for name in names:
-	username = name.split("/")
-	if ((username[0] not in usernames)):
-		usernames.append(username[0])
-print(usernames)
-'''
+print('\nDONE')
+output.write('\nDONE')
