3192ce90e7d6847653e7c9f2f4399c15c33c585e
alexklibisz <aklibisz@gmail.com>
2015-11-20 19:36:39 +0000

    small ui refinements

--- webapp/lib/relationships.py
+++ webapp/lib/relationships.py
@@ -1,3 +1,12 @@
 
+'''
+Take a list of feature repos and user repos.
+Return a matched list.
+The matched list consists of 
+1) any feature_repos that are in user_repos
+2) any feature_repos that have a relationship > 0 with user_repos
+'''
 def match_repos(feature_repos, user_repos):
+
+
   return feature_repos
