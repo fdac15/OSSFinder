ec9d263ef30f103d73850f1567979177592c2b03
Alex Klibisz <aklibisz@gmail.com>
2015-11-20 15:16:24 -0500

    Update relationships.py

--- webapp/lib/relationships.py
+++ webapp/lib/relationships.py
@@ -5,6 +5,9 @@ source = client['ossfinder']['rel_aggregate']
 
 '''
 Take a list of feature repos and user repos.
+feature_repos are the repos that are returned from a feature search (step 1 in the webapp).
+user_repos are the repos that the user selected (step 2 in the webapp).
+
 Create a matched list.
 The matched list consists of 
 1) any feature_repos that are in user_repos
@@ -12,7 +15,7 @@ The matched list consists of
 Sort the list by relationship value
 '''
 def match_repos(feature_repos, user_repos):
-  
+  matched = []
 
 
-  return feature_repos
+  return matched
