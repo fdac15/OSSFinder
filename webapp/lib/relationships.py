73bfd26ab94f6538781d019f35f45c1afe046ac2
Alex Klibisz <aklibisz@gmail.com>
2015-11-20 15:22:54 -0500

    Update relationships.py

--- webapp/lib/relationships.py
+++ webapp/lib/relationships.py
@@ -9,13 +9,16 @@ feature_repos are the repos that are returned from a feature search (step 1 in t
 user_repos are the repos that the user selected (step 2 in the webapp).
 
 Create a matched list.
-The matched list consists of 
-1) any feature_repos that are in user_repos
-2) any feature_repos that have a relationship > 0 with user_repos
+The matched list consists of any feature_repos that have a relationship > 0 with user_repos
 Sort the list by relationship value
 '''
 def match_repos(feature_repos, user_repos):
   matched = []
+  
+  # Find stuff
+  
+  # Put stuff in the matched list
+  
 
 
   return matched
