fc61113e2f318f3329229d6b0b221b617db304d5
mbenkhayal <mbenkhayal@users.noreply.github.com>
2015-11-25 04:01:55 +0000

    fixed bugs, will run more tests

--- relationship-calculations/aggregate_calculate.py
+++ relationship-calculations/aggregate_calculate.py
@@ -31,14 +31,14 @@ def aggregate_calculate(source, target, multiplier, name):
 			#add a new doc with the repos, value, and total
 	for rel in curlist:
 		tot = rel[name] * multiplier
-		aggregate.save(rel)
+		#aggregate.save(rel)
 		if(aggregate.find({'repo_a' : rel['repo_a']}).count() > 0): #rel a is f1 a
 			for f1 in aggregate.find({'repo_a':rel['repo_a']}):
 				if(rel['repo_b'] == f1['repo_b']):
 					aggregate.update_one({'_id':f1['_id']}, {"$inc":{name:1}})
 					aggregate.update_one({'_id':f1['_id']}, {"$inc":{'total':tot}})
 					#out = str(['U', rel['repo_a'], rel['repo_b'], name])
-					print("if 1")
+					#print("if 1")
 					break
 		elif(aggregate.find({'repo_b': rel['repo_a']}).count() > 0): #rel a is f1 b
 			for f1 in aggregate.find({'repo_b':rel['repo_a']}):
@@ -46,7 +46,7 @@ def aggregate_calculate(source, target, multiplier, name):
 					aggregate.update_one({'_id':f1['_id']}, {"$inc":{name:1}})
 					aggregate.update_one({'_id':f1['_id']}, {"$inc":{'total':tot}})
 					#out = str(['U', rel['repo_a'], rel['repo_b'], name])
-					print("if 2")
+					#print("if 2")
 					break
 		elif(aggregate.find({'repo_a': rel['repo_b']}).count() > 0): #rel b is f1 a
 			for f1 in aggregate.find({'repo_a':rel['repo_b']}):
@@ -54,7 +54,7 @@ def aggregate_calculate(source, target, multiplier, name):
 					aggregate.update_one({'_id':f1['_id']}, {"$inc":{name:1}})
 					aggregate.update_one({'_id':f1['_id']}, {"$inc":{'total':tot}})
 					#out = str(['U', rel['repo_a'], rel['repo_b'], name])
-					print("if 3")
+					#print("if 3")
 					break
 		elif(aggregate.find({'repo_b': rel['repo_b']}).count() > 0): #rel b is f1 b
 			for f1 in aggregate.find({'repo_b':rel['repo_b']}):
@@ -62,10 +62,10 @@ def aggregate_calculate(source, target, multiplier, name):
 					aggregate.update_one({'_id':f1['_id']}, {"$inc":{name:1}})
 					aggregate.update_one({'_id':f1['_id']}, {"$inc":{'total':tot}})
 					#out = str(['U', rel['repo_a'], rel['repo_b'], name])
-					print("if 4")
+					#print("if 4")
 					break
 		else: #add a new one to the aggregate relation
-			newrel = {'repo_a':rel['repo_a'], 'repo_b':rel['repo_b'], name:rel['name'], total:tot}
+			newrel = {'repo_a':rel['repo_a'], 'repo_b':rel['repo_b'], name:rel[name], 'total':tot}
 			aggregate.save(newrel)
-			out = str(['C', rel['repo_a'], rel['repo_b'], name])
-			print(out)
+			#out = str(['C', rel['repo_a'], rel['repo_b'], name])
+			#print(out)
