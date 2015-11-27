1d540fcae3d6fe6e431edb2036b827944b45be9a
mbenkhayal <mbenkhayal@users.noreply.github.com>
2015-11-27 23:11:41 +0000

    threaded watcher relations seems to be broken

--- relationship-calculations/watcher-relationships-threaded.py
+++ relationship-calculations/watcher-relationships-threaded.py
@@ -19,27 +19,27 @@ users = watchers.distinct("login")
 # of documents. In our case this will be the aggregate_calculate
 # function. 
 
-def worker_function(chunk, relationships, users, output}:
+def worker_function(chunk, relationships, users, output):
 	for user in users:
-	userWatchers = watchers.find({"login": user}).distinct("full_name")
-	if(len(userWatchers) > 1):
-		for i in range(0, len(userWatchers)):
-			for j in range(i+1, len(userWatchers)):
-				rel = relationships.find({'repo_a': userWatchers[i], 'repo_b': userWatchers[j]})
-				rel = list(rel)
-				if(len(rel) > 0):
-					rel = rel[0]
-					rel['watchers'] = rel['watchers'] + 1
-					out = str(['U', userWatchers[i], userWatchers[j]])
-					print(out)
-					output.write(out)
-					relationships.update_one({'_id': rel['_id']}, {"$inc": {"watchers":1}})
-				else:
-					rel = {'repo_a': userWatchers[i], 'repo_b': userWatchers[j], 'watchers': 1}
-					out = str(['C', userWatchers[i], userWatchers[j]])
-					print(out)
-					output.write(out)
-					relationships.save(rel)
+		userWatchers = watchers.find({"login": user}).distinct("full_name")
+		if(len(userWatchers) > 1):
+			for i in range(0, len(userWatchers)):
+				for j in range(i+1, len(userWatchers)):
+					rel = relationships.find({'repo_a': userWatchers[i], 'repo_b': userWatchers[j]})
+					rel = list(rel)
+					if(len(rel) > 0):
+						rel = rel[0]
+						rel['watchers'] = rel['watchers'] + 1
+						out = str(['U', userWatchers[i], userWatchers[j]])
+						print(out)
+						output.write(out)
+						relationships.update_one({'_id': rel['_id']}, {"$inc": {"watchers":1}})
+					else:
+						rel = {'repo_a': userWatchers[i], 'repo_b': userWatchers[j], 'watchers': 1}
+						out = str(['C', userWatchers[i], userWatchers[j]])
+						print(out)
+						output.write(out)
+						relationships.save(rel)
 
 worker_args = {
 	'relationships': relationships,
