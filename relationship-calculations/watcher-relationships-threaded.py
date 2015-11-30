b9915bab1ffb8f8da39ff01ae3de62dc2982bb2f
mbenkhayal <mbenkhayal@users.noreply.github.com>
2015-11-30 06:23:46 +0000

    finished threaded watchers, running now

--- relationship-calculations/watcher-relationships-threaded.py
+++ relationship-calculations/watcher-relationships-threaded.py
@@ -2,7 +2,7 @@ from helpers import pymongo_worker_threads as workers
 import aggregate_calculate as agr
 import pymongo
 
-output = open('watcher-output.txt', 'w')
+output = open('watcher-output-threaded.txt', 'w')
 
 client = pymongo.MongoClient(host="da0.eecs.utk.edu")
 
@@ -26,26 +26,31 @@ def worker_function(chunk, relationships, users, output):
 	for doc in chunk:
 		if doc['login'] in watchers:
 			tmp = watchers[doc['login']]
-			print("tmp before append: ")
-			print(tmp)
+			#print("tmp before append: ")
+			#print(tmp)
 			tmp.append(doc['full_name'])
-			print("tmp after append: ")
-			print(tmp)
+			#print("tmp after append: ")
+			#print(tmp)
 			watchers[doc['login']] = tmp
-			print("watchers[doc['login']]: ")
-			print(watchers[doc['login']])
-			#watchers[doc['login']] = 'ayylmao';
-			#print(doc['login'])
-		#watchers['foo'] = 'bar'
-		#print(doc['login'])
-			#watchers[doc['login']].append(doc['full_name'])
-	#print(watchers['foo'])
-	print(watchers)
-
-'''
-	for key, vals in watchers.items():
-		print(key, vals)
-'''		
+	for user in users:
+		if(len(watchers[user]) > 1):
+			for i in range(0, len(watchers[user])):
+				for j in range(i+1, len(watchers[user])):
+					rel = relationships.find({'repo_a': watchers[user][i], 'repo_b': watchers[user][j]})
+					rel = list(rel)
+					if(len(rel) > 0):
+						rel = rel[0]
+						rel['watchers'] = rel['watchers'] + 1
+						out = str(['U', watchers[user][i], watchers[user][j]])
+						print(out)
+						output.write(out)
+						relationships.update_one({'_id':rel['_id']}, {"$inc": {"watchers":1}})
+					else:
+						rel = {'repo_a': watchers[user][i], 'repo_b': watchers[user][j], 'watchers': 1}
+						out = str(['C', watchers[user][i], watchers[user][j]])
+						print(out)
+						output.write(out)
+						relationships.save(rel)
 	
 '''
 	for user in users:
@@ -69,6 +74,8 @@ def worker_function(chunk, relationships, users, output):
 						output.write(out)
 						relationships.save(rel)
 '''
+	
+
 
 worker_args = {
 	'relationships': relationships,
@@ -77,5 +84,5 @@ worker_args = {
 }
 
 # Call the module function with the correct params named
-workers.do_work(source_collection = watchers, worker_function = worker_function, find_args = {}, worker_args = worker_args, num_docs_per_thread = 10000, wait_to_join = true)
+workers.do_work(source_collection = watchers, worker_function = worker_function, find_args = {}, worker_args = worker_args, num_docs_per_thread = 10000, wait_to_join = False)
 
